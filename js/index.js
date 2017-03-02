92f990dba89f1e59b294585b40c995bc3ce80176
zhangyuecl <2698051674@qq.com>
2017-03-02 20:54:42 +0800

    首页tab层切换

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 $(function(){
-	$('.nav li').on('click',function(){
-		$('.nav ul li').removeClass('active-nav')
+	$('#nav li').on('click',function(){
+		$('#nav ul li').removeClass('active-nav')
 		$(this).addClass('active-nav');
+		var index=$(this).index();
+		$('.slider').css({"-webkit-transform":"translateX(-"+25*index+"%)"});
 	});
 });
-
