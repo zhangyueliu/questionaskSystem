34a4ad865e7a9acee7cfbaff1de1d57a122373ba
zhangyuecl <2698051674@qq.com>
2017-03-07 19:09:53 +0800

    我的问答有问题

---
+++ js/qa-center.js
@@ -0,0 +1,28 @@
+$(function(){
+	$('.my-question').on('tap',function(){
+		$('.my-active').css({'transform':'translateX(0%)'});
+		$('.slider').css({'transform':'translateX(0%)'});
+	});
+	$('.my-qa').on('tap',function(){
+		$('.my-active').css({'transform':'translateX(100%)'});
+		$('.slider').css({'transform':'translateX(-50%)'});
+	});
+	$(window).scroll(function(){
+		console.log(1)
+		//change-div距离浏览器的高度
+		var top=document.getElementsByClassName('change-div')[0].offsetTop-document.body.scrollTop;
+		//头部的高度
+		var headerheight=$('.head-fixed').height();
+		//change-div的高度
+		var changeheight=$('.change-div').height();
+		if(top<headerheight){
+			$('.change-div').css({'position':'fixed','top':'4rem','z-index':'10'});
+		}
+		//列表容器的高度
+		var slidertop=document.getElementsByClassName('slider')[0].offsetTop-document.body.scrollTop;
+		if(headerheight+changeheight<slidertop){
+			$('.change-div').css({'position':'relative','top':'0','z-index':'1'})
+		}
+	});
+	$('body').goTop();
+})
